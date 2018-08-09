7e40756b63d41feae4ac541164568eb7cf5dd4fd
1031080441 <1031080441@qq.com>
2018-08-09 18:24:47 +0800

    pickle模块.2018.8.9

---
+++ aboutpickler.py
@@ -0,0 +1,13 @@
+import pprint, pickle
+
+pkl_file = open("data1.pkl", "rb")
+
+data1 = pickle.load(pkl_file)
+
+pprint.pprint(data1)
+
+data2 = pickle.load(pkl_file)
+
+pprint.pprint(data2)
+
+pkl_file.close()
