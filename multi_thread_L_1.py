25791ab5723b0af50297e3153e7de49c711f8ac7
1031080441 <1031080441@qq.com>
2018-08-16 20:41:23 +0800

    python3 多线程 2018.8.16

---
+++ multi_thread_L_1.py
@@ -0,0 +1,17 @@
+import _thread, time
+
+def print_time(threadName, delay):
+	count = 0
+	while count < 5:
+		time.sleep(delay)
+		count += 1
+		print("%s : %s count : %d" % (threadName, time.ctime(time.time()), count))
+
+try:
+	_thread.start_new_thread(print_time, ("Thread-1", 2, ))
+	_thread.start_new_thread(print_time, ("Thread-2", 4, ))
+except:
+	print("Error:无法启动线程")
+
+while 1:
+	pass
---
+++ multi_thread_L_2.py
@@ -0,0 +1,34 @@
+import threading, time
+
+exitFlag = 0
+
+class myThread(threading.Thread):
+	def __init__(self, threadID, name, counter):
+		threading.Thread.__init__(self)
+		self.threadID = threadID
+		self.name = name
+		self.counter = counter
+
+	def run(self):
+		print("开始线程：" + self.name)
+		print_time(self.name, self.counter, 5)
+		print("退出线程：" + self.name)
+
+def print_time(threadName, delay, counter):
+	while counter:
+		if exitFlag:
+			threadName.exit()
+		time.sleep(delay)
+		print("{} : {}".format(threadName, time.ctime(time.time())))
+		counter -= 1
+
+thread1 = myThread(1, "Thread-1", 1)
+thread2 = myThread(2, "Thread-2", 2)
+
+thread1.start()
+thread2.start()
+thread1.join()
+print("hwt1")
+thread2.join()
+print("hwt2")
+print("退出主线程")
